112a106ed6359bb52e1ef972e11a84c80f241057
Nepumi-Jr <peerator@kkumail.com>
2024-08-28 20:50:57 +0700

    update evaluate select command

--- StandardJudge/compare_select.py
+++ StandardJudge/compare_select.py
@@ -49,9 +49,37 @@ def compare(cursor, solution_sql:str, user_sql:str, solution_db:str, user_db:str
     if solution_tag.is_ignore_error and is_solution_error:
         return ResultDto("-", 0, 1, elapsed * 1000, "This is not an error")
     
-    if solution_result == user_result:
+    # starting to compare the result
+    cur_score, max_score = 0, 0
+    penalty = 0
+    for i, v in enumerate(solution_result):
+        if i >= len(user_result):
+            max_score += len(v)
+            break
+        
+        for j, vv in enumerate(v):
+            if j >= len(user_result[i]):
+                max_score += len(v) - j
+                break
+
+            if vv == user_result[i][j]:
+                cur_score += 1
+            max_score += 1
+
+        penalty += len(user_result[i]) - len(v)
+    
+    if len(user_result) > len(solution_result):
+        for i in range(len(solution_result), len(user_result)):
+            penalty += len(user_result[i])
+    
+    final_score = max(0 , cur_score - penalty)
+
+
+    if final_score == max_score:
         result = ResultDto("P", 1, 1, elapsed * 1000, "Correct result")
+    elif final_score / max_score >= 0.5:
+        result = ResultDto("H", final_score / max_score, 1, elapsed * 1000, f"Correct {cur_score} out of {max_score} (penalty: -{penalty})")
     else:
-        result = ResultDto("-", 0, 1, elapsed * 1000, "Wrong result")
+        result = ResultDto("-", 0, 1, elapsed * 1000, f"Wrong result (Correct {cur_score} out of {max_score} penalty: -{penalty}")
 
     return result
